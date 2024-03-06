e0e78c80f8ab12b6fdc750e5ec1d7d559e415e44
Saniago Horton <santiagohorton@gmail.com>
2024-03-06 17:34:02 -0300

    agrego la carpeta views con el index.handlebars y la carpeta layouts junto con el archivo main.handlebars

--- src/app.js
+++ src/app.js
@@ -3,16 +3,26 @@ const app = express();
 const PUERTO = 8080;
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+const exphbs = require("express-handlebars"); // Corrected typo
+
+// Configuring Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+// Routing
+app.use("/", viewsRouter);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Rutas:
+// Routes
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-// Middleware para manejar errores
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
